ada6a06e97bb5cce9b4a35fc0bee25e83888dd79
Al <alexkhien@hotmail.com>
2021-04-06 18:24:22 -0400

    first commit

---
+++ popup/popup.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function() {
+    // your button here
+    var link = document.getElementById('test');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+        var newURL = "http://youtube.com/";
+        chrome.tabs.create({ url: newURL });
+    });
+});
