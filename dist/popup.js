abd48c91d36969bbfa90842023167e3854a79f0f
Al <alexkhien@hotmail.com>
2021-04-16 17:39:51 -0400

    Minor format/comment editing

--- dist/popup.js
+++ dist/popup.js
@@ -1,83 +1,10 @@
-// document.querySelector('#sign-in').addEventListener('click', function () {
-//     chrome.runtime.sendMessage({ message: 'login' }, function (response) {
-//         if (response === 'success') {
-//             window.close(); 
-//         }
-//     });
-// });
-
-// document.querySelector('button').addEventListener('click', function () {
-//     chrome.runtime.sendMessage({ message: 'isUserSignedIn' }, function (response) {
-//         // alert(response);
-//     });
-// })
-
-
-// const CLIENT_ID = '912099096037-dqcnfr7r6na9gcj66p2k6k73s1ubf3i5.apps.googleusercontent.com'
-// const DISCOVERY_DOCS = 'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest'
-// const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly'
-
-// const authorizeButton = document.getElementById('authorize-button')
-// const signoutButton = document.getElementById('signout-button')
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     onload="this.onload(){}";
-//     handleClientLoad();
-//     onreadystatechange="if (this.readyState === 'complete') this.onload()";
-// })
-
-// // Load auth2 library
-// const handleClientLoad = () => {
-//     gapi.load('client:auth2', initClient);
-// }
-
-// // Init API client library and sest up sign in listeners
-// function initClient() {
-//     gapi.client.init({
-//         disoveryDocs: DISCOVERY_DOCS,
-//         clientId: CLIENT_ID,
-//         scope: SCOPES
-//     }).then(() => {
-//         //Listen for sign in state changes
-//         gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-//         // Handle initial sign in state
-//         updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-//         authorizeButton.onclick = handleAuthClick;
-//         signoutButton.onclick = handleSignoutClick;
-//     })
-// }
-
-// // Update UI sign in state changes
-// const updateSigninStatus = (isSignedIn) => {
-//     if (isSignedIn) {
-//         authorizeButton.style.display = 'none';
-//         signoutButton.style.display = 'block';
-//     } else {
-//         authorizeButton.style.display = 'block';
-//         signoutButton.style.display = 'none';
-//     }
-// }
-
-// function handleAuthClick() {
-//     gapi.auth2.getAuthInstance().signIn();
-// }
-
-// function handleSignoutClick() {
-//     gapi.auth2.getAuthInstance().signOut();
-// }
-
-
-
 // returns a number between 0 and max - 1
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-
-
 window.addEventListener('DOMContentLoaded', function () {
     var apiKey = config.YOUTUBE_API_KEY2;
-    // const apiCall = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCWiY6fYdxuEe78r-0uFCnhA&maxResults=3&fields=(items%2Fsnippet%2Fthumbnails%2Fdefault%2Furl)%2C%20(items%2Fsnippet%2Ftitle)%2C%20(items%2Fsnippet%2Fdescription)%2C%20(items%2Fid%2FvideoId)&key=' + apiKey;
 
     const cases = document.querySelector(".cases")
 
@@ -95,11 +22,8 @@ window.addEventListener('DOMContentLoaded', function () {
     var sugg2 = document.getElementById("sugg2");
     var sugg3 = document.getElementById("sugg3");
 
-
     //(items/snippet/thumbnails/default/url), (items/snippet/title), (items/snippet/description), (items/id/videoId)
-
     var sortCategories = ["date", "rating", "relevance", "title", "viewCount", "viewCount"];
-
 
     chrome.storage.sync.get({ list: [] }, (data) => {
 
@@ -108,10 +32,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
         var randomCatIndex = getRandomInt(sortCategories.length);
         var randomVidIndex = [getRandomInt(25), getRandomInt(25), getRandomInt(25)];
-        // var randomChannelIndex = getRandomInt(storedChannelIds.length);
         var randomChannelIndex = [getRandomInt(storedChannelIds.length), getRandomInt(storedChannelIds.length), getRandomInt(storedChannelIds.length)]
-
-        // cases.textContent = randomCatIndex + "," + randomVidIndex + "," + randomChannelIndex;
 
         // returns api stirng
         function getApiString(i) {
@@ -174,55 +95,6 @@ window.addEventListener('DOMContentLoaded', function () {
                 })
             })
     })
-
-
-    // fetch(apiCall)
-    //     .then(res => {
-    //         if (res.status !== 200) {
-    //             cases.textContent = 'STATUS RETURN ERROR';
-    //             sugg1img.src = "../images/error.png";
-    //             sugg2img.src = "../images/error.png";
-    //             sugg3img.src = "../images/error.png";
-
-    //             sugg1title.textContent = "Error generating videos";
-    //             return;
-    //         }
-    //         res.json().then(data => {
-    //             // cases.textContent = JSON.stringify(data.items[0].id.videoId);
-
-    //             //data.items[0].snippet.thumbnails.default.url - video thumbnail url
-    //             //data.items[0].snippet.title
-    //             //data.items[0].snippet.description
-    //             //data.items[0].id.videoId
-
-    //             sugg1img.src = data.items[0].snippet.thumbnails.default.url;
-    //             sugg1title.textContent = data.items[0].snippet.title;
-    //             sugg1desc.textContent = data.items[0].snippet.description;
-
-    //             sugg2img.src = data.items[1].snippet.thumbnails.default.url;
-    //             sugg2title.textContent = data.items[1].snippet.title;
-    //             sugg2desc.textContent = data.items[1].snippet.description;
-
-    //             sugg3img.src = data.items[2].snippet.thumbnails.default.url;
-    //             sugg3title.textContent = data.items[2].snippet.title;
-    //             sugg3desc.textContent = data.items[2].snippet.description;
-
-    // sugg1.addEventListener("click", () => {
-    //     window.open(`https://www.youtube.com/watch?v=${data.items[0].id.videoId}`, '_blank').focus();
-    // })
-
-    //             sugg2.addEventListener("click", () => {
-    //                 window.open(`https://www.youtube.com/watch?v=${data.items[1].id.videoId}`, '_blank').focus();
-    //             })
-
-    //             sugg3.addEventListener("click", () => {
-    //                 window.open(`https://www.youtube.com/watch?v=${data.items[2].id.videoId}`, '_blank').focus();
-    //             })
-    //         })
-    //     })
-    //     .catch(function (err) {
-    //         cases.textContent = 'OTHER ERROR';
-    //     })
 
     return true;
 });
